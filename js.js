1153ba54c2f5ff4dd1adfcfb7780c399bc267d26
brokenhoax <brokenhoax@outlook.com>
2020-07-14 20:01:54 -0500

    Added CSS Grid, Enabled Search Functionality, Practice Page

--- js.js
+++ js.js
@@ -1,22 +1,29 @@
 $( document ).ready(function() {
 
-    $('.search-button').click(function searchWord() {
-        var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://wordsapiv1.p.rapidapi.com/words/hatchback/definitions",
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-                "x-rapidapi-key": "f9e00ad0b5msh6a8c9fd337bf3e0p11e193jsn21f3a1d12552"
-            }
+  $('.search-button').click(function searchWord() {
+    const searchButton = document.querySelector('.search-button');
+    var inputValue = document.getElementById('search-input');
+    var value = inputValue.value;
+    console.log(value)
+
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://wordsapiv1.p.rapidapi.com/words/" + value + "/definitions",
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+            "x-rapidapi-key": "f9e00ad0b5msh6a8c9fd337bf3e0p11e193jsn21f3a1d12552"
         }
-        
-        $.ajax(settings).then(function (response) {
-            console.log(response);
-            $(".word").text(JSON.stringify(response.word).replaceAll("\"", ""));
-            console.log(response.word);
-            $(".definition").text(JSON.stringify(response.definitions[0].definition).replaceAll("\"", ""));
-        });
+    }
+    
+    $.ajax(settings).then(function (response) {
+        console.log(response);
+        $(".word").text(JSON.stringify(response.word).replaceAll("\"", ""));
+        console.log(response.word);
+        $(".definition").text(JSON.stringify(response.definitions[0].definition).replaceAll("\"", ""));
     });
+
+  });
+
 });
