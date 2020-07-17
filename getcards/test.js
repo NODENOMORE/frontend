d010378818e2c4f9680b01cd8d75c31e36104ba5
brokenhoax <brokenhoax@outlook.com>
2020-07-17 00:57:55 -0500

    Updated Flash Cards, SelectAll, Insert

---
+++ getcards/test.js
@@ -0,0 +1,38 @@
+  // Select All From MySQL Database
+
+$( document ).ready(function() {
+
+  $('#card1').click(function searchWord() {
+
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "http://localhost:9000/mywords",
+      "method": "GET",
+    }
+    
+    $.ajax(settings).then(function (response) {
+
+      console.log(response);
+
+      // // $(".category").text(JSON.stringify(response.words[0].category).replaceAll("\"", ""));
+
+      // for (const property in response) {
+      // console.log(`${property}: ${object[property]}`);
+      // }
+      // // $(".definition").text(JSON.stringify(response.definitions[0].definition).replaceAll("\"", ""));
+      // // Looping through each result item
+
+      for (var i = 0; i <= response._word.length; i++) {
+        
+        console.log(i)
+        var category = response._word[i].category;
+        $("#category"+[i]).html(category);
+
+      }
+
+    });
+    
+  });
+
+});
